785ef5b1475c7136bf1304c0bfd07a1963454a60
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-09-01 10:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+  output.innerHTML = ''; // Clear previous images
+
+  const downloadPromises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+
+      img.onload = () => {
+        output.appendChild(img);
+        resolve();
+      };
+
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+    });
+  });
+
+  Promise.all(downloadPromises)
+    .then(() => {
+      console.log("All images downloaded successfully.");
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
